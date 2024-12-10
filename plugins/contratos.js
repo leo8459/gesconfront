d1bf866b9e13c9de9169d48978e5c03c7ce974e2
Leo <joseaguilar987654321@gmail.com>
2024-12-10 15:19:28 -0400

    rutas

--- plugins/contratos.js
+++ plugins/contratos.js
@@ -9,7 +9,7 @@ export default function ({ $axios, store, redirect }, inject) {
       }
     });
     // let url ='http://172.65.10.52:8450/contratos/'
-    let url ='http://172.65.10.33:8450/contratos/'
+    let url ='http://172.65.10.33:8459/contratos/'
 
     // const url = 'http://localhost/backgescon2/public/contratos/';
     contratos.setBaseURL(url);
