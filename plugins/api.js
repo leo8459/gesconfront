ef88ad3343f4ab5e2d6630813da6dbee4b2fbf52
leo8459 <92004716+leo8459@users.noreply.github.com>
2026-08-21 13:28:57 -0400

    Salir por ip nomas

--- plugins/api.js
+++ plugins/api.js
@@ -8,11 +8,9 @@ export default function ({ $axios, store, redirect }, inject) {
     }
   });
   // let url ='http://172.65.10.52:8450/carteros/'
-  // let url ='http://172.65.10.33:8459/carteros/'
-let url ='https://gescon.correos.gob.bo:8459/carteros/'
-  // const url = 'http://localhost/backgescon2/public/carteros/';
-        // const url = 'https://gescon.correos.gob.bo/carteros/';
+  let url ='http://172.65.10.33:8459/carteros/'
 
+  // const url = 'http://localhost/backgescon2/public/carteros/';
   api.setBaseURL(url);
 
   api.interceptors.request.use(config => {
